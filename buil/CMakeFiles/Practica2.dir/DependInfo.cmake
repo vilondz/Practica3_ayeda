2885cdb3c3af75689ff10e81d007876354f7f09f
vilondz <alu0101573052@ull.edu.es>
2026-03-02 22:57:54 +0000

    añadi las hormigas pero todavia no comen

---
+++ buil/CMakeFiles/Practica2.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/ant.cc" "CMakeFiles/Practica2.dir/ant.cc.o" "gcc" "CMakeFiles/Practica2.dir/ant.cc.o.d"
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/ant_Cx.cc" "CMakeFiles/Practica2.dir/ant_Cx.cc.o" "gcc" "CMakeFiles/Practica2.dir/ant_Cx.cc.o.d"
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/ant_Hx.cc" "CMakeFiles/Practica2.dir/ant_Hx.cc.o" "gcc" "CMakeFiles/Practica2.dir/ant_Hx.cc.o.d"
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/main.cc" "CMakeFiles/Practica2.dir/main.cc.o" "gcc" "CMakeFiles/Practica2.dir/main.cc.o.d"
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/simulator.cc" "CMakeFiles/Practica2.dir/simulator.cc.o" "gcc" "CMakeFiles/Practica2.dir/simulator.cc.o.d"
+  "/home/vboxuser/Documents/practicas/practica3/Practica3_ayeda/tape.cc" "CMakeFiles/Practica2.dir/tape.cc.o" "gcc" "CMakeFiles/Practica2.dir/tape.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ buil/CMakeFiles/Practica2.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practica2.dir/ant.cc.o"
+  "CMakeFiles/Practica2.dir/ant.cc.o.d"
+  "CMakeFiles/Practica2.dir/ant_Cx.cc.o"
+  "CMakeFiles/Practica2.dir/ant_Cx.cc.o.d"
+  "CMakeFiles/Practica2.dir/ant_Hx.cc.o"
+  "CMakeFiles/Practica2.dir/ant_Hx.cc.o.d"
+  "CMakeFiles/Practica2.dir/main.cc.o"
+  "CMakeFiles/Practica2.dir/main.cc.o.d"
+  "CMakeFiles/Practica2.dir/simulator.cc.o"
+  "CMakeFiles/Practica2.dir/simulator.cc.o.d"
+  "CMakeFiles/Practica2.dir/tape.cc.o"
+  "CMakeFiles/Practica2.dir/tape.cc.o.d"
+  "Practica2"
+  "Practica2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practica2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
